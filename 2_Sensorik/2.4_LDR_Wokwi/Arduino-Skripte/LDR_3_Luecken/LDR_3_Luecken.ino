838b72db8cf50acf514bba32a7de37aa3a6e5c82
ElenaRichter99 <elena.richter@htwk-leipzig.de>
2026-04-14 13:58:08 +0200

    Korrekturen in LDR Wokwi Beispiel 3

--- 2_Sensorik/2.4_LDR_Wokwi/Arduino-Skripte/LDR_3_Luecken/LDR_3_Luecken.ino
+++ 2_Sensorik/2.4_LDR_Wokwi/Arduino-Skripte/LDR_3_Luecken/LDR_3_Luecken.ino
@@ -1,15 +1,15 @@
-# include <Wire.h> 
-# include <LuquidCrystal_I2C.h> // Bibliothek muss zuvor installiert werden 
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h> // Bibliothek muss zuvor installiert werden 
 
 #define LDR_PIN 34
 #define LCD_ADDR 0x27 // I2C-Adresse LCD (typisch 0x27)
 #define LCD_COLS 16
 #define LCD_ROWS 2
 
-LiquidCrystal_I2C_lcd(________, ________; ________);
+LiquidCrystal_I2C_lcd(________, ________, ________);
 
 void setup() {
-  ldf.init();
+  lcd.init();
   lcd.backlight();
   Serial.begin(115200); // Baud-Rate Serieller Monitor
 }
@@ -28,6 +28,7 @@ void loop() {
 
   delay(5000);
 }
+
 
 
 
